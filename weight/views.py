4fcc97b4d9fd72b559e8c80957803706c382a607
monsiorpawel <monsiorpawel@gmail.com>
2019-09-18 23:10:32 +0200

    Add modelForm

--- weight/views.py
+++ weight/views.py
@@ -1,6 +1,19 @@
 from django.shortcuts import render
+from .forms import WeightForm
 from .models import Weight
+from django.contrib.auth.models import User
+from django.utils import timezone
 
 
 def index(request):
-    return render(request, 'weight/weight.html')
+    instance = Weight(username=request.user, date=timezone.now())
+
+    if request.method == 'POST':
+        form = WeightForm(data=request.POST, instance=instance)
+        if form.is_valid():
+            form.username = User
+            form.date = timezone.now()
+            form.save()
+
+    form = WeightForm()
+    return render(request, 'weight/weight.html', {'form': form})

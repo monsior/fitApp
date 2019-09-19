59653259b94882da52f3284be9b352c8ae590f30
monsiorpawel <monsiorpawel@gmail.com>
2019-09-19 21:42:54 +0200

    Add test for created objects

--- weight/tests.py
+++ weight/tests.py
@@ -1,10 +1,25 @@
 from django.test import TestCase
 from .models import Weight
+from django.utils import timezone
+from django.contrib.auth.models import User
+from django.core.validators import MinValueValidator
 
 
 class WeightModelTests(TestCase):
 
-    def test_is_weight_positive(self):
+    def setUp(self):
+        self.u = User.objects.create(username='user1')
+
+    def test_is_weight_object_created(self):
         """
-        weight value should be positive
+        check if weight object is created
         """
+        w = Weight()
+        w.weight = 73.5
+        w.date = timezone.now()
+        w.username = self.u
+        w.save()
+        self.assertEqual(w, Weight.objects.last())
+
+    def tearDown(self):
+        self.u.delete()

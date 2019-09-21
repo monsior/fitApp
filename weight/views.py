a001f0634a8aa0663e0fa5c96d16f3448373adda
monsiorpawel <monsiorpawel@gmail.com>
2019-09-21 16:54:56 +0200

    Index function split into two functions

--- weight/views.py
+++ weight/views.py
@@ -6,7 +6,15 @@ from django.utils import timezone
 from django.contrib import messages
 
 
-def index(request):
+def display_weights(request):  # return a dictionary containing every user's weight
+    form = WeightForm()
+    weights = Weight.objects.filter(username=request.user)
+    weights = reversed(weights)  # we want to display weights chronologically
+    args = {'form': form, 'weights': weights}
+    return args
+
+
+def add_weight(request):
     instance = Weight(username=request.user, date=timezone.now())
 
     if request.method == 'POST':
@@ -20,8 +28,4 @@ def index(request):
         else:
             messages.warning(request, 'Weight NOT added!')
 
-    form = WeightForm()
-    weights = Weight.objects.filter(username=request.user)
-    weights = reversed(weights)  # we want to display weights chronologically
-    args = {'form': form, 'weights': weights}
-    return render(request, 'weight/weight.html', args)
+    return render(request, 'weight/weight.html', display_weights(request))

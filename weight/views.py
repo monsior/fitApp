b3b21315376964f7ec02f846523477a3a9257ef7
monsiorpawel <monsiorpawel@gmail.com>
2019-09-22 15:45:58 +0200

    Add a weight chart

--- weight/views.py
+++ weight/views.py
@@ -10,7 +10,12 @@ def display_weights(request):  # return a dictionary containing every user's wei
     form = WeightForm()
     weights = Weight.objects.filter(username=request.user)
     weights = reversed(weights)  # we want to display weights chronologically
-    args = {'form': form, 'weights': weights}
+    weight_values = []
+    date_labels = []
+    for weight in weights:
+        weight_values.append(int(weight.weight))
+        date_labels.append(str(weight.date.day) + "-" + str(weight.date.month) + "-" + str(weight.date.year))
+    args = {'form': form, 'weight_values': weight_values, 'date_labels': date_labels}
     return args
 
 

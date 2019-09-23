a7b708a827861d703fab9e41aca9122cc8c33706
monsiorpawel <monsiorpawel@gmail.com>
2019-09-23 19:33:32 +0200

    Add a date field

--- weight/forms.py
+++ weight/forms.py
@@ -2,8 +2,13 @@ from django import forms
 from .models import Weight
 
 
+class DateInput(forms.DateInput):
+    input_type = 'date'
+
+
 class WeightForm(forms.ModelForm):
 
     class Meta:
         model = Weight
-        fields = {'weight'}
+        widgets = {'date': DateInput()}
+        fields = {'weight', 'date'}
--- weight/models.py
+++ weight/models.py
@@ -1,12 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.core.validators import MinValueValidator
+from datetime import date
 
 
 class Weight(models.Model):
     username = models.ForeignKey(User, on_delete=models.CASCADE)
     weight = models.DecimalField(max_digits=4, decimal_places=1, validators=[MinValueValidator(0)])
-    date = models.DateField()
+    date = models.DateField(default=date.today)
 
     def __str__(self):
         return str(self.date) + ' ' + str(self.username)
